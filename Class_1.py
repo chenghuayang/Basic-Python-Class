acc1e6a2a79e4784eb50f4a8b4f79a878853f4e5
chenghuayang <5032148+chenghuayang@users.noreply.github.com>
2021-07-30 21:24:48 +0800

    update

--- Class_1.py
+++ Class_1.py
@@ -1,1 +1,31 @@
-print(5 + 5 + 5)
+# assign
+box_1 = 10
+box_2 = 20
+
+#Boolean:  true or False
+penchan_is_penguin = True
+nekokun_is_penguin = False
+
+print(penchan_is_penguin == True)
+# True is True ?
+
+print(penchan_is_penguin == False)
+# True is False ?
+
+print(nekokun_is_penguin == True)
+# False is True ?
+
+print(nekokun_is_penguin == False)
+# False is False ?
+
+# Boolean2
+print(box_1 == box_2)
+print(box_1 + 10 == box_2)
+print(box_1 + 30 == box_2 + 20)
+print(box_1 + 10 <= box_2)
+
+# Type
+print(type(10))  # integer
+print(type(10.0))  # floating number
+print(10 == 10.0)
+print(type(penchan_is_penguin))  # Boolean`
